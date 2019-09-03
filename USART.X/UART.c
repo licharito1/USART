f29baf1aab3d5d497e4c412f0f90c7f3124e08f2
Usuario <Usuario@LAPTOP-NCMTBGV4>
2019-09-03 16:59:20 -0300

    primera version prueba

---
+++ USART.X/UART.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   UART.c
+ * Author: Usuario
+ *
+ * Created on 3 de septiembre de 2019, 16:58
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
